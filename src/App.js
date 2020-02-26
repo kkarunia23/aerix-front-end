8f1b4db4d45250dbd3cd186d311008c73f1329cf
kkarunia23 <kenneth.karunia@gmail.com>
2020-02-26 19:11:32 +0800

    frontend

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Toggle from './components/toggle.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Toggle />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/toggle.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Toggle extends Component {
+    render() {
+        return (
+            <h1>Toggle</h1>
+        )
+    }
+}
+
+export default Toggle;
+
