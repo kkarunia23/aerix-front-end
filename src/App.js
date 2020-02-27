bb0194e10220317a799e07347cae3c52bdebf2d7
kkarunia23 <kenneth.karunia@gmail.com>
2020-02-27 16:11:32 +0800

    update route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import Toggle from './components/toggle.js'
 
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Toggle />
-      </div>
+      <Toggle />
     );
   }
 }
--- src/components/toggle.js
+++ src/components/toggle.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
 
 class Toggle extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            feeds: []
+        }
+
+    }
+    updateFeed = (feed, index) => {
+        console.log(this.state.feeds[0].isActive);
+        feed.isActive = !feed.isActive;
+
+        fetch('http://localhost:3000/feeds/' + feed._id, {
+            body: { isActive: !this.state.feeds[0].isActive },
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(updatedFeed => updatedFeed.json())
+            .then(jsonedFeed => {
+                fetch('http://localhost:3000/feeds/')
+                    .then(response => response.json())
+                    .then(feeds => {
+                        this.setState({ feeds: feeds })
+                    })
+            })
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3000/feeds')
+            .then(response => response.json())
+            .then(feeds => {
+                this.setState({
+                    feeds: feeds
+                });
+            })
+    }
+
     render() {
         return (
-            <h1>Toggle</h1>
+            <table>
+                <tbody>
+                    {this.state.feeds.map(feed => {
+                        return (
+                            <tr>
+                                <td> {feed.name} |</td>
+                                <td onClick={() => this.updateFeed(feed)}> {feed.isActive ? "active" : "inactive"} </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         )
     }
 }
